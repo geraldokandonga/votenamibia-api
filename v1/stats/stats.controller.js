490789d3f39e31893e4bf332b643202ae614db03
Geraldo Kandonga <10811063+geraldokandonga@users.noreply.github.com>
2020-12-06 12:01:31 +0200

    Update stats.controller.js
    change map to forLoop

--- v1/stats/stats.controller.js
+++ v1/stats/stats.controller.js
@@ -27,49 +27,55 @@ exports.countAll = catchAsync(async(req, res, next) => {
 
 
 /** count region constituencies and polling stations */
-exports.countStatsPerRegion = catchAsync(
-  async (req, res, next) => {
-    // step 1)  find region
-    const query = Region.findById(req.params.id);
+exports.countStatsPerRegion = catchAsync(async (req, res, next) => {
+  // step 1)  find region
+  const query = Region.findById(req.params.id);
 
-    const region = await query;
+  const region = await query;
 
-    if (!region) {
-      return next(new AppError("No region found.", 404));
-    }
-
-    // step 2) count regions constituencies
-    const totalConstituencies = await Constituency.countDocuments(
-      { region: region.id },
-      function(err, count) {
-        if (err) return next(new AppError("Something went wrong!", 400));
-        return count;
-      }
-    );
-
-    // step 3) loop through all constituencies to find the one that match the region and return its id and pass it to get polling station count
-    const constituencies = await Constituency.find(
-      { region: region.id },
-      function(err, doc) {
-        // if error return
-        if (err) return;
-        return doc;
-      }
-    );
-
-    let totalPollingStations = 0;
-
-    constituencies.map(async doc => {
-      const total = await PollingStation.find({ constituency: doc._id });
-      if (total.length) return (totalPollingStations += total.length);
-    });
-
-    res.status(201).json({
-      status: "success",
-      data: {
-        constituencies: totalConstituencies,
-        pollingStations: totalPollingStations
-      }
-    });
+  if (!region) {
+    return next(new AppError("No region found.", 404));
   }
-);
+
+  // step 2) count regions constituencies
+  const totalConstituencies = await Constituency.countDocuments(
+    { region: region.id },
+    function(err, count) {
+      if (err) return next(new AppError("Something went wrong!", 400));
+      return count;
+    }
+  );
+
+  // step 3) Find all constituencies in region
+
+  const constituencies = await Constituency.find(
+    { region: region.id },
+    function(err, docs) {
+      // if error return
+      if (err) return;
+
+      return docs;
+    }
+  );
+
+  let totalPollingStations = 0;
+
+  // step 4) loop through all the constituencies to find polling stations within
+  for (let i = 0; i < constituencies.length; i++) {
+    await PollingStation.countDocuments(
+      { constituency: constituencies[i]._id },
+      function(err, count) {
+        totalPollingStations += count; // add
+      }
+    );
+  }
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      region,
+      constituencies: totalConstituencies,
+      pollingStations: totalPollingStations
+    }
+  });
+});
